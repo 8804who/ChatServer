5d9eb4b7a3de437b6b9c57051e545189c1d50d93
1a2a3a4a5 <8804who@naver.com>
2021-11-11 23:29:13 +0900

    동빈나 유튜브 보고 따라해봄

---
+++ src/main/java/com/chatserver/chatserver/Client.java
@@ -0,0 +1,63 @@
+package com.chatserver.chatserver;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+
+public class Client {//클라이언트와 통신을 하기 위해 필요한 기능 정의
+    Socket socket;//소켓 통신을 위한 라이브러리
+
+    public Client(Socket socket){
+        this.socket=socket;
+        receive();
+    }
+    public void receive(){//클라이언트로부터 메시지를 전달 받는 메소드
+        Runnable thread=new Runnable() {
+            @Override
+            public void run() {
+                try{
+                    while (true){
+                        InputStream in = socket.getInputStream();
+                        byte[] buffer = new byte[512];
+                        int length = in.read(buffer);
+                        while(length==-1) throw new IOException();
+                        System.out.println("[메시지 수신 성공]"+socket.getRemoteSocketAddress()+": "+Thread.currentThread().getName());
+                        String message = new String(buffer, 0, length, "UTF-8");
+                        for(Client client : Main.clients){
+                            client.send(message);
+                        }
+                    }
+                }catch(Exception e){
+                    try{
+                        System.out.print("[메시지 수신 오류]"+socket.getRemoteSocketAddress()+": "+Thread.currentThread().getName());
+                    } catch (Exception e2){
+                    e2.printStackTrace();
+                    }
+                }
+            }
+        };
+        Main.threadPool.submit(thread);
+    }
+    public void send(String message){//클라이언트로 메시지를 전달하는 메소드
+        Runnable thread = new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    OutputStream out = socket.getOutputStream();
+                    byte[] buffer = message.getBytes("UTF-8");
+                    out.write(buffer);//버퍼에 담긴 내용을 클라이언트로 전송
+                    out.flush();
+                } catch (Exception e) {
+                    try{
+                        System.out.println("[메시지 송신 오류]"+socket.getRemoteSocketAddress()+": "+Thread.currentThread().getName());
+                        Main.clients.remove(Client.this);//오류 발생시 오류 발생한 클라이언트를 서버에서 제거
+                        socket.close();
+                    } catch (Exception e2){
+                        e2.printStackTrace();
+                    }
+                }
+            }
+        };
+    }
+}
---
+++ src/main/java/com/chatserver/chatserver/Main.java
@@ -0,0 +1,122 @@
+package com.chatserver.chatserver;
+
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.text.Font;
+import javafx.stage.Stage;
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Iterator;
+import java.util.Vector;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Main extends Application {
+    public static ExecutorService threadPool;//스레드를 관리하는 라이브러리
+    public static Vector<Client> clients = new Vector<Client>();
+
+    ServerSocket serverSocket;
+    
+    public void startServer(String IP, int port){//서버를 구동시키는 메소드
+        try{
+            serverSocket = new ServerSocket();
+            serverSocket.bind(new InetSocketAddress(IP, port));
+        } catch (IOException e) {
+            e.printStackTrace();
+            if(!serverSocket.isClosed()){
+                stopServer();
+            }
+            return;
+        }
+        Runnable thread = new Runnable() {//클라이언트가 접속할 때까지 기다리는 쓰레드
+            @Override
+            public void run() {
+                while(true){
+                    try{
+                        Socket socket = serverSocket.accept();
+                        clients.add(new Client(socket));
+                        System.out.println("[클라이언트 접속] "+socket.getRemoteSocketAddress()+": "+Thread.currentThread().getName());
+                    } catch (IOException e) {
+                        if(!serverSocket.isClosed()){
+                            stopServer();
+                        }
+                        break;
+                    }
+                }
+            }
+        };
+        threadPool = Executors.newCachedThreadPool();
+        threadPool.submit(thread);
+    }
+
+    private void stopServer() {
+        try{//현재 작동중인 모든 소켓 닫기
+            Iterator<Client> iterator = clients.iterator();
+            while(iterator.hasNext()){
+                Client client = iterator.next();
+                client.socket.close();
+                iterator.remove();
+            }
+            if(serverSocket != null && !serverSocket.isClosed()){//서버 소켓 객체 닫기
+                serverSocket.close();
+            }
+            if(threadPool != null && !threadPool.isShutdown()){//쓰레드 풀 종료하기
+                threadPool.shutdown();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void start(Stage primaryStage) throws IOException {//프로그램 진입점
+        BorderPane root = new BorderPane();
+        root.setPadding(new Insets(5));
+
+        TextArea textArea = new TextArea();
+        textArea.setEditable(false);
+        textArea.setFont(new Font("나눔고딕",15));
+        root.setCenter(textArea);
+
+        Button toggleButton = new Button("시작하기");
+        toggleButton.setMaxWidth(Double.MAX_VALUE);
+        BorderPane.setMargin(toggleButton, new Insets(1,0,0,0));
+        root.setBottom(toggleButton);
+
+        String IP = "127.0.0.1";
+        int port = 9876;
+
+        toggleButton.setOnAction(event->{
+            if(toggleButton.getText().equals("시작하기")){
+                startServer(IP,port);
+                Platform.runLater(()->{
+                    String message = String.format("[서버 시작]\n", IP, port);
+                    textArea.appendText(message);
+                    toggleButton.setText("종료하기");
+                });
+            } else{
+                stopServer();
+                Platform.runLater(()->{
+                    String message = String.format("[서버 종료]\n", IP, port);
+                    textArea.appendText(message);
+                    toggleButton.setText("시작하기");
+                });
+            }
+        });
+        Scene scene = new Scene(root,400,400);
+        primaryStage.setTitle("[ 채팅 서버 ]");
+        primaryStage.setOnCloseRequest(event->stopServer());
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
